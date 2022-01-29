8a3d3b6b015d1f09cfe9a864c87ba231da66c136
e <ericoporto2008@gmail.com>
2022-01-29 19:47:15 -0300

    pyqtSignal as Signal to prepare a switch to pyside6 eventually

--- src/agstoolbox/at_tasks.py
+++ src/agstoolbox/at_tasks.py
@@ -1,5 +1,6 @@
 from __future__ import annotations  # for python 3.8
-from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtCore import QThread
+from PyQt6.QtCore import pyqtSignal as Signal
 
 from agstoolbox.core.settings import ConstSettings
 from agstoolbox.core.ags.get_game_projects import list_game_projects_in_dir
@@ -7,9 +8,9 @@ from agstoolbox.core.gh.list_releases import list_releases
 
 
 class ProjUpdateThread(QThread):
-    update_started = pyqtSignal()
-    update_ended = pyqtSignal()
-    update_canceled = pyqtSignal()
+    update_started = Signal()
+    update_ended = Signal()
+    update_canceled = Signal()
     proj_list = None
 
     def __init__(self):
@@ -33,9 +34,9 @@ def do_update_projects(update_ended, update_canceled):
 
 
 class ToolsUpdateThread(QThread):
-    update_started = pyqtSignal()
-    update_ended = pyqtSignal()
-    update_canceled = pyqtSignal()
+    update_started = Signal()
+    update_ended = Signal()
+    update_canceled = Signal()
     tools_list = None
 
     def __init__(self):
