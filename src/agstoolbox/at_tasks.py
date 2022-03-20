fe2a52ab73f0dd09b4fc48e77ed3c92187a6e9ee
e <ericoporto2008@gmail.com>
2022-03-20 15:59:15 -0300

    add methods for actually downloading a release

--- src/agstoolbox/at_tasks.py
+++ src/agstoolbox/at_tasks.py
@@ -3,6 +3,9 @@ from PyQt6.QtCore import QThread
 from PyQt6.QtCore import pyqtSignal as Signal
 
 from agstoolbox.core.ags.get_local_ags_editors import list_ags_editors_in_dir_list
+from agstoolbox.core.gh.download_release import download_release_to_cache
+from agstoolbox.core.gh.install_release import install_release_from_cache
+from agstoolbox.core.gh.release import Release
 from agstoolbox.core.settings import ConstSettings, Settings
 from agstoolbox.core.ags.get_game_projects import list_game_projects_in_dir, \
     list_game_projects_in_dir_list
@@ -113,5 +116,32 @@ def do_update_tools_managed(update_ended, update_canceled):
     thread = ToolsUpdateThreadManaged()
     thread.update_ended.connect(update_ended)
     thread.update_canceled.connect(update_canceled)
+    thread.start()
+    return thread
+
+
+# actual download
+class DownloadAManagedToolThread(QThread):
+    update_started = Signal()
+    update_ended = Signal()
+    release: Release = None
+
+    def __init__(self, release: Release = None):
+        QThread.__init__(self)
+        self.release = release
+
+    def run(self) -> None:
+        self.update_started.emit()
+        download_release_to_cache(self.release)
+        install_release_from_cache(self.release)
+        self.update_ended.emit()
+
+
+def do_download_managed(release: Release, update_ended=None, update_started=None):
+    thread = DownloadAManagedToolThread(release)
+    if update_ended is not None:
+        thread.update_ended.connect(update_ended)
+    if update_started is not None:
+        thread.update_started.connect(update_started)
     thread.start()
     return thread
--- src/agstoolbox/core/gh/install_release.py
+++ src/agstoolbox/core/gh/install_release.py
@@ -11,7 +11,7 @@ from agstoolbox.core.utils.file import remove_dir_contents
 def install_release_from_cache(release: Release):
     filepath_in_cache = get_zip_archive_cache_path(release)
     base_install_dir = Settings().get_editor_install_dir()
-    instal_dir = os.path.join(base_install_dir, release.version)
+    instal_dir = os.path.join(base_install_dir, release.version.as_str)
     Path(instal_dir).mkdir(parents=True, exist_ok=True)
     remove_dir_contents(instal_dir)
     with zipfile.ZipFile(filepath_in_cache, 'r') as zip_ref:
