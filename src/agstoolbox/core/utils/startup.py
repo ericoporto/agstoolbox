fedcfd0d10507c5d2492ca8e556a3a0518b9ddeb
e <ericoporto2008@gmail.com>
2023-04-02 21:17:25 -0300

    fix failures with other oses

--- src/agstoolbox/core/utils/startup.py
+++ src/agstoolbox/core/utils/startup.py
@@ -1,22 +1,23 @@
 from __future__ import annotations
 
 import os
+import sys
 
 from agstoolbox.core.utils.win_registry import set_run_key, get_run_key
 
 
-def _win_unsafe_remove_app_at_startup(app_name: str):
+def _win32_unsafe_remove_app_at_startup(app_name: str):
     set_run_key(app_name, None)
 
 
-def _win_remove_app_at_startup(app_name: str):
+def _win32_remove_app_at_startup(app_name: str):
     try:
-        _win_unsafe_remove_app_at_startup(app_name)
+        _win32_unsafe_remove_app_at_startup(app_name)
     except OSError:
         print("could not remove app from startup")
 
 
-def _win_set_app_at_startup(app_name: str, app_path: str) -> bool:
+def _win32_set_app_at_startup(app_name: str, app_path: str) -> bool:
     set_failed = False
 
     try:
@@ -26,7 +27,7 @@ def _win_set_app_at_startup(app_name: str, app_path: str) -> bool:
 
     if set_failed:
         try:
-            _win_unsafe_remove_app_at_startup(app_name)
+            _win32_unsafe_remove_app_at_startup(app_name)
         except OSError:
             set_failed = True
     else:
@@ -36,13 +37,38 @@ def _win_set_app_at_startup(app_name: str, app_path: str) -> bool:
     return not set_failed
 
 
+def _linux_remove_app_at_startup(app_name: str):
+    print("stub _linux_remove_app_at_startup")
+
+
+def _linux_set_app_at_startup(app_name: str, app_path: str) -> bool:
+    print("stub _linux_set_app_at_startup")
+    return False
+
+
+def _darwin_remove_app_at_startup(app_name: str):
+    print("stub _darwin_remove_app_at_startup")
+
+
+def _darwin_set_app_at_startup(app_name: str, app_path: str) -> bool:
+    print("stub _darwin_set_app_at_startup")
+    return False
+
+
 def remove_app_at_startup(app_name: str):
-    if os.name == 'nt':
-        _win_remove_app_at_startup(app_name)
+    _remove_app_at_startup = {
+        'darwin': _darwin_remove_app_at_startup,
+        'linux': _linux_remove_app_at_startup,
+        'win32': _win32_remove_app_at_startup}
+
+    _remove_app_at_startup[sys.platform](app_name)
 
 
 def set_app_at_startup(app_name: str, app_path: str) -> bool:
-    if os.name == 'nt':
-        return _win_set_app_at_startup(app_name, app_path)
+    _set_app_at_startup = {
+        'darwin': _darwin_set_app_at_startup,
+        'linux': _linux_set_app_at_startup,
+        'win32': _win32_set_app_at_startup}
 
-    return False
+    return _set_app_at_startup[sys.platform](app_name, app_path)
+
