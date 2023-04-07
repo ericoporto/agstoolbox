af6d8a7da14ed059890762af630e68ff7dbad89b
e <ericoporto2008@gmail.com>
2023-04-07 19:15:48 -0300

    fix typo

--- src/agstoolbox/core/gh/install_release.py
+++ src/agstoolbox/core/gh/install_release.py
@@ -10,8 +10,8 @@ from agstoolbox.core.utils.file import remove_dir_contents, mkdirp
 def install_release_from_cache(release: Release):
     filepath_in_cache = get_zip_archive_cache_path(release)
     base_install_dir = Settings().get_editor_install_dir()
-    instal_dir = os.path.join(base_install_dir, release.version.as_str)
-    remove_dir_contents(instal_dir)
-    mkdirp(instal_dir)
+    install_dir = os.path.join(base_install_dir, release.version.as_str)
+    remove_dir_contents(install_dir)
+    mkdirp(install_dir)
     with zipfile.ZipFile(filepath_in_cache, 'r') as zip_ref:
-        zip_ref.extractall(instal_dir)
+        zip_ref.extractall(install_dir)
