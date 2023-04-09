88f5ac46b1d7ad07a4704beb96d43956fe790fcb
e <ericoporto2008@gmail.com>
2023-04-09 00:31:48 -0300

    avoid violating column limit

--- tests/test_gh.py
+++ tests/test_gh.py
@@ -18,21 +18,23 @@ cur_dir = Path(__file__).resolve().parent
 def test_parse_releases():
     f = open(os.path.join(cur_dir, 'resources/gh_releases.json'))
     response_json = json.load(f)
-    releases = parse_releases(response_json)
-    assert len(releases) == 90
-    assert releases[43].tag == "v.3.6.0.15"
-    assert releases[44].tag == "v.3.6.0.14"
-    assert releases[46].tag == "v.3.6.0.13"
-    assert releases[46].name == "v.3.6.0.13 - Alpha 14"
-    assert releases[46].url == "https://api.github.com/repos/adventuregamestudio/ags/releases/56166627"
-    assert releases[46].archive_name == "AGS-3.6.0.13-Alpha14.zip"
-    assert releases[46].archive_url == \
-           "https://github.com/adventuregamestudio/ags/releases/download/v.3.6.0.13/AGS-3.6.0.13-Alpha14.zip"
-    assert releases[46].archive_size == 25219327
-    assert releases[0].tag == "v.3.6.0.47"
-    assert releases[0].name == "3.6.0 Release"
-    assert releases[0].url == "https://api.github.com/repos/adventuregamestudio/ags/releases/97715961"
-    assert releases[0].archive_name == "AGS-3.6.0.47.zip"
-    assert releases[0].archive_url == \
-           "https://github.com/adventuregamestudio/ags/releases/download/v.3.6.0.47/AGS-3.6.0.47.zip"
-    assert releases[0].archive_size == 40475597
+    r = parse_releases(response_json)
+    assert len(r) == 90
+    assert r[43].tag == "v.3.6.0.15"
+    assert r[44].tag == "v.3.6.0.14"
+    assert r[46].tag == "v.3.6.0.13"
+    assert r[46].name == "v.3.6.0.13 - Alpha 14"
+    assert r[46].url == "https://api.github.com/repos/adventuregamestudio/ags/releases/56166627"
+    assert r[46].archive_name == "AGS-3.6.0.13-Alpha14.zip"
+    assert r[46].archive_url == \
+           "https://github.com/" \
+           "adventuregamestudio/ags/releases/download/v.3.6.0.13/AGS-3.6.0.13-Alpha14.zip"
+    assert r[46].archive_size == 25219327
+    assert r[0].tag == "v.3.6.0.47"
+    assert r[0].name == "3.6.0 Release"
+    assert r[0].url == "https://api.github.com/repos/adventuregamestudio/ags/releases/97715961"
+    assert r[0].archive_name == "AGS-3.6.0.47.zip"
+    assert r[0].archive_url == \
+           "https://github.com/" \
+           "adventuregamestudio/ags/releases/download/v.3.6.0.47/AGS-3.6.0.47.zip"
+    assert r[0].archive_size == 40475597
