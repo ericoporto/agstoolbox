3067a9155c2b8b924690bff0e895950ea75b543f
e <ericoporto2008@gmail.com>
2024-01-05 23:38:14 -0300

    core: add ags multifile class

---
+++ src/agstoolbox/core/ags/multifile.py
@@ -0,0 +1,9 @@
+from __future__ import annotations  # for python 3.8
+
+
+class MultiFile:
+    name: str | None = None
+    filename: str | None = None
+    datafile: int | None = None
+    length: int | None = None
+    offset: int | None = None
