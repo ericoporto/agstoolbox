8eb683129533bc6f3bb7dcad5d7890ad51a5363f
e <ericoporto2008@gmail.com>
2024-07-19 01:12:48 -0300

    core: add method to create template from game dir

--- src/agstoolbox/core/ags/ags_template.py
+++ src/agstoolbox/core/ags/ags_template.py
@@ -3,6 +3,9 @@ from __future__ import annotations  # for python 3.8
 from typing import List
 from pathlib import Path
 
+from agstoolbox.core.ags.datafile_writer import get_multifile_lib, make_data_file_from_multifile_lib
+from agstoolbox.core.ags.multifilelib import MultiFileLib
+
 
 def get_dir_file_list(directory: str, file_mask: str, search_option: str = 'top'):
     try:
@@ -43,3 +46,15 @@ def construct_template_filelist(parent_dir: str) -> List[str]:
     add_matching_files(files_to_include, "*.pdf", parent_dir)
     add_matching_files(files_to_include, "*.ogv", parent_dir)
     return files_to_include
+
+
+def create_template_from_game_dir(project_dir: str, template_filepath: str):
+    if Path(template_filepath).exists():
+        Path(template_filepath).unlink()
+
+    template_filename: str = Path(template_filepath).name
+    template_dir: str = str(Path(template_filepath).parent.as_posix())
+
+    filelist: List[str] = construct_template_filelist(project_dir)
+    mlib: MultiFileLib = get_multifile_lib(filelist, project_dir, template_filename, False)
+    make_data_file_from_multifile_lib(mlib, template_dir)
