21937c85d6f910278b6b53eb6bf0663cdec434cf
e <ericoporto2008@gmail.com>
2024-07-20 21:43:21 -0300

    core: fix warnings in ags_local_run

--- src/agstoolbox/core/ags/ags_local_run.py
+++ src/agstoolbox/core/ags/ags_local_run.py
@@ -1,8 +1,5 @@
 from __future__ import annotations  # for python 3.8
 
-import os
-from subprocess import Popen
-
 from agstoolbox.core.ags.ags_editor import LocalAgsEditor
 from agstoolbox.core.ags.game_project import GameProject
 from agstoolbox.core.utils.file import get_absolute_path
@@ -21,4 +18,3 @@ def ags_editor_load(editor: LocalAgsEditor, project: GameProject, block: bool = 
 def ags_editor_build(editor: LocalAgsEditor, project: GameProject, block: bool = False) -> int:
     project_path = get_absolute_path(project.path)
     return run_exe_params(editor.path, block, ['/compile', project_path])
-
