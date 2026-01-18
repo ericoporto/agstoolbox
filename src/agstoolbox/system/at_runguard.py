2bcdb2758c464610a6db41a4511fa5ad464bb9f4
Érico Porto <ericoporto2008@gmail.com>
2026-01-18 11:32:51 -0300

    system: fix runguard to also work on macOS

--- src/agstoolbox/system/at_runguard.py
+++ src/agstoolbox/system/at_runguard.py
@@ -7,7 +7,16 @@ class RunGuard:
 
     def __init__(self, parent: QtCore.QObject, key: str):
         self._shared = QtCore.QSharedMemory(key, parent)
+
+        # If we can attach, another instance is running
+        # This is safe on macOS and still works on Windows
+        # Should work on Linux too, but haven't tested yet
+        if self._shared.attach():
+            print('This app is already running!')
+            self.unique = False
+            return
+
         self.unique = self._shared.create(512, QtCore.QSharedMemory.AccessMode.ReadWrite)
 
         if not self.unique:
-            print('This app is already running!')
+            print('ERROR: Failed to create shared memory: ' + self._shared.errorString())
